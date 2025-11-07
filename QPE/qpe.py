c840094c2062e6956bf779d18b4c686f5257146f
minjoon-park <d2ochgm@gmail.com>
2025-11-06 21:44:29 -0800

    QPE

---
+++ QPE/qpe.py
@@ -0,0 +1,41 @@
+import math
+
+from qiskit import QuantumCircuit, transpile
+from qiskit.circuit import Instruction
+from qiskit.circuit.library import QFT, CPhaseGate, XGate
+from qiskit_aer import AerSimulator
+
+
+def qpe(
+    precision: int,
+    c_unitary: QuantumCircuit | Instruction,
+    initializer: QuantumCircuit | Instruction | None = None,
+) -> dict:
+    num_state_qubits = c_unitary.num_qubits - 1
+    state_qubits = list(range(precision, precision + num_state_qubits))
+    ancilla_qubits = list(range(precision))
+
+    qpe = QuantumCircuit(precision + num_state_qubits, precision)
+    if initializer:
+        qpe.compose(initializer, state_qubits, inplace=True)
+
+    qpe.h(ancilla_qubits)
+
+    for ancilla_qubit in ancilla_qubits:
+        qpe.compose(c_unitary.power(2**ancilla_qubit), [ancilla_qubit] + state_qubits, inplace=True)
+
+    qpe.barrier()
+
+    qpe.compose(QFT(precision, inverse=True), ancilla_qubits, inplace=True)
+
+    qpe.barrier()
+
+    for digit in ancilla_qubits:
+        qpe.measure(digit, digit)
+
+    aer_sim = AerSimulator()
+    return aer_sim.run(transpile(qpe, aer_sim), shots=1024).result().get_counts()
+
+
+qpe(3, CPhaseGate(2 * math.pi * (1 / 4 + 1 / 8)), XGate())
+# {'101': 1024}
